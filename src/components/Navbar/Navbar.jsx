1ac7384f2c5f56963f120cb27d2dacf0566cda27
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2022-03-21 20:35:16 +0100

    first commit - navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <>
+        <div className="container-navbar">
+            <div className="nav-principal contenedor">
+                <a href="#">Inicio</a>
+                <a href="#">Sobre Nosotros</a>
+                <a href="#">Contacto</a>
+            </div>
+        </div>
+    </>
+  )
+}
